2143fe8d21377348c81685269d815f91ca4a4416
Kumar, Hrithik  <hrithik.kumar@edmentum.com>
2025-01-06 21:24:11 +0530

    NewComponent

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';  
+import { HttpClientModule } from '@angular/common/http';
+import { ResultComponent } from './result/result.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, 
+  { path: '', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },  
+  { path: 'result', component: ResultComponent},
   { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- UI/src/app/home/home.component.ts
+++ UI/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';  // Import Router
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {}  // Inject Router
+
+  navigateToQuestion(): void {
+    console.log('Navigating to question page'); // Add this for debugging
+    this.router.navigate(['/question']);
+  }
 }
